ce9bb2a5a1ddff90d62279d86c5cf837f45a44ef
Dmitry <dmeyerson@runtitle.com>
2015-10-04 19:55:42 -0500

    take money

---
+++ data_dl.sh
@@ -0,0 +1,4 @@
+wget https://drivendata.s3.amazonaws.com/data/8/public/bee_images.zip
+wget https://drivendata.s3.amazonaws.com/data/8/public/b37d3960-6909-472b-9ce1-c33b07dbdb66.csv
+wget https://drivendata.s3.amazonaws.com/data/8/public/SubmissionFormat.csv
+wget https://drivendata.s3.amazonaws.com/data/8/public/2f9cb1e7-d1ab-43aa-b04c-1264eea88a2f.zip
