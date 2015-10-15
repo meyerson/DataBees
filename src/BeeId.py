48b56cee0c3ad2d21078fc2808c9fa8f879489d7
Dmitry <dmeyerson@runtitle.com>
2015-10-15 10:16:13 -0500

    fuckgin bees

--- src/BeeId.py
+++ src/BeeId.py
@@ -6,11 +6,16 @@ import pandas as pd
 from settings import base
 import glob
 import os
-from BeeId_helper import img_to_matrix
+from BeeId_helper import img_to_matrix, flatten_image
+from sklearn.decomposition import RandomizedPCA
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 
 
 class ImageNorm(BaseTask):
 
+    seed = luigi.Parameter(default=1)
     ndocs = luigi.IntParameter(default=10)
     test_dir = luigi.Parameter(default='test')
     train_dir = luigi.Parameter(default='train')
@@ -25,12 +30,42 @@ class ImageNorm(BaseTask):
         #unzip ndocs
         print((os.path.join(base.BEEHOME,self.train_dir+'/*')))
         images = [x for x in glob.glob(os.path.join(base.BEEHOME,self.train_dir+'/*'))]
+        #print(images)
+        #images  = map(lambda x: img_to_matrix(x),images)
+        images = [img_to_matrix(x) for x in images[0:10]]
+          
 
-        img_to_matrix(images[0])
+        data = []
+        for i,image in enumerate(images):
 
-        #normalize one at a time
+            img = flatten_image(image)
+            print(img[1:5])
 
-        #save somewhere
+            print(img.shape)
+            data.append(img)
+
+        # data = np.array(data)
+
+        print(data[0].shape)
+
+
+
+        pca = RandomizedPCA(n_components=2)
+
+        X = pca.fit_transform(data)
+
+        print(X.shape)
+        df = pd.DataFrame({"x": X[:, 0], "y": X[:, 1]}) #, "label":np.where(y==1, "check", "driver's license")})
+        colors = ["red", "yellow"]
+        #for label, color in zip(df['label'].unique(), colors):
+        for color in colors:
+            plt.scatter(df['x'], df['y'], c=color) 
+            #plt.scatter(df[mask]['x'], df[mask]['y'], c=color) #, label=label)
+        plt.legend()
+        plt.show()
+
+
+
 
 if __name__ == '__main__':
     luigi.run()
--- src/BeeId_helper.py
+++ src/BeeId_helper.py
@@ -1,14 +1,23 @@
 from PIL import Image
+import numpy as np
 
 def img_to_matrix(filename, verbose=False):
     """
     takes a filename and turns it into a numpy array of RGB pixels
     """
     img = Image.open(filename)
-    if verbose==True:
-        print "changing size from %s to %s" % (str(img.size), str(STANDARD_SIZE))
     
-    img = list(img.getdata())
-    img = map(list, img)
-    img = np.array(img)
-    return img
+    img = list(map(list,list(img.getdata())))
+    return np.array(img)
+
+def flatten_image(img):
+    """
+    takes in an (m, n) numpy array and flattens it
+    into an array of shape (1, m * n)
+    """
+    print(img)
+    s = img.shape[0] * img.shape[1]
+    print(s)
+    img_wide = [(x[0],x[1],x[2]) for x  in img.reshape(1, s)]
+
+    return img_wide[0]
